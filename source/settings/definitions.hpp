a29d3747c3f3083506fe31f23c0c9e5c0921afcd
Matheus Braga <matheus.mtb7@gmail.com>
2016-05-03 00:48:13 -0300

    Unix headers

--- source/settings/definitions.hpp
+++ source/settings/definitions.hpp
@@ -34,7 +34,7 @@
 /*
     Lua
 */
-#define DISABLE_LUAINTERFACE
+//#define DISABLE_LUAINTERFACE
 
 
 
@@ -62,7 +62,7 @@
 #define LUA_INCLUDE <stdio.h>
 #define DISABLE_LUAINTERFACE
 
-#else
+#elif _WIN32
 #define BEAR_SDL_CONST_INIT SDL_INIT_EVERYTHING
 #define BEAR_SDL_IMAGE_CONST_INIT IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF
 
@@ -71,6 +71,16 @@
 #define SDL_TTF_LIB_HEADER "SDL2/SDL_ttf.h"
 #define SDL_MIXER_LIB_HEADER "SDL2/SDL_mixer.h"
 #define LUA_INCLUDE <lua.hpp>
+#else
+#define BEAR_SDL_CONST_INIT SDL_INIT_EVERYTHING
+#define BEAR_SDL_IMAGE_CONST_INIT IMG_INIT_JPG | IMG_INIT_PNG | IMG_INIT_TIF
+
+#define SDL_LIB_HEADER "SDL2/SDL.h"
+#define SDL_IMAGE_LIB_HEADER "SDL2/SDL_image.h"
+#define SDL_TTF_LIB_HEADER "SDL2/SDL_ttf.h"
+#define SDL_MIXER_LIB_HEADER "SDL2/SDL_mixer.h"
+#define LUA_INCLUDE <lua5.1/lua.hpp>
+
 #endif
 
 
