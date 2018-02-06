c5cbd2d6a38dce2c406f4137937e5d4b18b956a5
Matheus Braga <matheus.mtb7@gmail.com>
2018-02-06 03:10:10 -0200

    debug message handling for opengl

--- source/framework/debughelper.cpp
+++ source/framework/debughelper.cpp
@@ -3,6 +3,15 @@
 #include <iostream>
 #include <stdio.h>
 
+#ifdef RENDER_OPENGLES
+    #define GL_GLEXT_PROTOTYPES 1
+    #include GLES_LIB
+#endif // RENDER_OPENGLES
+
+#ifdef RENDER_OPENGL
+    #include GL_LIB
+#endif // RENDER_OPENGLES
+
 
 void DebugHelper::AssertAlloc(void *ptr,std::string file,std::string func,int line,std::string msg){
     if (ptr == NULL){
@@ -11,9 +20,32 @@ void DebugHelper::AssertAlloc(void *ptr,std::string file,std::string func,int li
     }
 }
 void DebugHelper::DisplayGlError(){
-    /*GLenum error = glGetError();
-    if( error != GL_NO_ERROR )
-    {
-        bear::out << "OpenGL error: " << error <<"\n";
-    }*/
+    GLenum err;
+	while ((err = glGetError()) != GL_NO_ERROR)
+	{
+		std::string name = "??????";
+		if (err == GL_INVALID_ENUM)
+			name = "GL_INVALID_ENUM";
+
+		if (err == GL_INVALID_VALUE)
+			name = "GL_INVALID_VALUE";
+
+		if (err == GL_INVALID_OPERATION)
+			name = "GL_INVALID_OPERATION";
+
+		if (err == GL_INVALID_FRAMEBUFFER_OPERATION)
+			name = "GL_INVALID_FRAMEBUFFER_OPERATION";
+
+		if (err == GL_OUT_OF_MEMORY)
+			name = "GL_OUT_OF_MEMORY";
+
+
+		if (err == GL_STACK_UNDERFLOW)
+			name = "GL_STACK_UNDERFLOW";
+
+		if (err == GL_STACK_OVERFLOW)
+			name = "GL_STACK_OVERFLOW";
+
+		bear::out << "GL error: " << name << "\n";
+	}
 }
