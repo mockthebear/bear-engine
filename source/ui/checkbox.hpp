488cae2a75aeca818bbf23a90cd65051b0c236c8
Mock <matheus.mtb7@gmail.com>
2018-04-18 08:46:21 -0300

    checkbox return the flag

--- source/ui/checkbox.hpp
+++ source/ui/checkbox.hpp
@@ -6,9 +6,13 @@ class Checkbox : public UIBase{
     public:
         Checkbox(Point pos,bool *check,std::string str,UIBase *owner);
         void Input();
+        void SetFlagAddr(bool *addr){
+            checked = addr;
+        };
         void Render(Point where=Point());
         void SetCheck(bool c);
         void Refresh();
+        std::function<void(UIBase*,bool)> OnCheck;
     private:
         bool *checked;
 };
