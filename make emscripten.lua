67a65a328de05221ac2410b1e22a16da76617787
Mock <matheus.mtb7@gmail.com>
2018-08-02 22:27:35 -0300

    script to build on emscripten

--- make emscripten.lua
+++ make emscripten.lua
@@ -5,18 +5,24 @@ local fileTypes = {"cpp","c"}
 local notAssets = {"hpp","h"}
 local ignore	= {"cbp","exe","depend","layout","rc","ico","rc","dll","so"}
 local COMPILER = "em++";
+local COMPILERC= "emcc";	
 local ASSETS_FOLDER = "game"
 local SOURCE_FOLDER = "source"
 local FILEOUT = "snakescape.html"
-local LDFLAGS = ""
-local PRELOADSTUFF = " --preload-file /game/data/ --preload-file /game/ui/"
-local CFLAGS = "-s ASSERTIONS=1 -DRENDER_OPENGL -s USE_SDL=2 -s USE_SDL_TTF=2 -s USE_VORBIS=1 --use-preload-plugins -s ALLOW_MEMORY_GROWTH=1 -std=c++11"
+local LDFLAGS = "--emrun"
+local PRELOADSTUFF = "--preload-file game/engine/ --preload-file game/lua/ --preload-file game/ui/ --preload-file game/data --preload-file game/test.burr --preload-file game/snd.burr --preload-file game/teste.txt"
+local CFLAGS 	= "-s ASSERTIONS=1 -DRENDER_OPENGL2 -DNEED_SHADER_LOCATION -s USE_SDL=2 -s USE_SDL_TTF=2 -s USE_VORBIS=1 --use-preload-plugins -s ALLOW_MEMORY_GROWTH=1 -std=c++11"
+local CCFLAGS 	= "-s ASSERTIONS=1 -DRENDER_OPENGL2 -DNEED_SHADER_LOCATION -s USE_SDL=2 -s USE_SDL_TTF=2 -s USE_VORBIS=1 --use-preload-plugins -s ALLOW_MEMORY_GROWTH=1"
 
 
 local OUTSTR = ""
 local FILES = "";
 local ASSETS = ""
 
+local ret = os.execute(COMPILER.." -v")
+if ret == 1 then
+	os.execute("D:\\emscripten\\emsdk\\emsdk_env.bat")
+end
 
 function hasFile(fname)
 	local f = io.open(fname,'r')
@@ -28,6 +34,7 @@ function hasFile(fname)
 end
 
 
+
 function checkFile(fname,root)
 	root = root or ''
 	local f = io.open(root..fname,'w')
@@ -36,16 +43,17 @@ function checkFile(fname,root)
 		if not hasFile(root..primaryDir.."/1.txt") then
 			os.execute("cd "..root.." && mkdir "..primaryDir)
 		else
-			os.execute("rm "..root..primaryDir.."/1.txt")
+			os.execute("del "..root..primaryDir.."/1.txt")
 		end
 		return checkFile(tail,root..primaryDir..'/')
 	else
-		os.execute("rm "..root..fname)
+		os.execute("del "..root..fname)
 		return true
 	end
 end
 
-
+local cmdLines = ""
+local owo = {}
 function parseFolderRecursively(Fold)
 	local f,er = io.open("source/beargine.cbp","r")
 	if f then
@@ -54,18 +62,30 @@ function parseFolderRecursively(Fold)
 			if i:find("%.cpp") then
 				local fileOut = "obj/emscripten/"..i:gsub("%.cpp",".bc")
 
-				if not hasFile(fileOut) then
-					checkFile(fileOut)
+				
+					--checkFile(fileOut)
 					local line = COMPILER.." -c source/"..i.." -o "..fileOut.." "..CFLAGS
 					FILES = FILES .. "obj/emscripten/"..i:gsub("%.cpp",".bc").." "
 					--check the avaliability of the dir
-					print("Now:",line)
-					local ret = os.execute(line)
+					--print("Now:",line)
+					cmdLines = cmdLines..fileOut.." "
+					owo[#owo+1] = line
+					--local ret = os.execute(line)
 					if ret == 1 then
 						return false
 					end
-				else
-					FILES = FILES .. "obj/emscripten/"..i:gsub("%.cpp",".bc").." "
+
+			elseif i:find("%.c$") then
+				local fileOut = "obj/emscripten/"..i:gsub("%.c$",".bc")
+				local line = COMPILERC.." -c source/"..i.." -o "..fileOut.." "..CCFLAGS
+				FILES = FILES .. "obj/emscripten/"..i:gsub("%.c",".bc").." "
+				--check the avaliability of the dir
+				--print("Now:",line)
+				cmdLines = cmdLines..fileOut.." "
+				owo[#owo+1] = line
+				--local ret = os.execute(line)
+				if ret == 1 then
+					return false
 				end
 			end
 		end
@@ -75,8 +95,34 @@ function parseFolderRecursively(Fold)
 end
 if parseFolderRecursively(SOURCE_FOLDER) then
 	local outp = FILES..' '..CFLAGS..' '..LDFLAGS.. ' '..PRELOADSTUFF
-	print("Ready files: ",outp)
-	os.execute("emcc -o kek.html "..outp)
+	--print("Ready files: ",outp)
+
+	outp = "emcc -o kek.html "..outp
+
+	--
+	local a = io.open("OWO.txt","w")
+
+	
+
+	for i,b in pairs(owo) do 
+		print("-->"..b)
+		local ret = os.execute(b)
+		a:write(b.."\r\n")
+		if ret == 1 then
+			return false
+		end
+	end
+
+	a:write(outp)
+	a:close()
+
+	print("\n"..outp)
+	local ret = os.execute(outp)
+	if ret == 1 then
+			return false
+	end
+
+	--os.execute("emrun kek.html")
 end
 
 --Build folder:
