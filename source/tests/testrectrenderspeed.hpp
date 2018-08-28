020316e0a4461a35800eed6617d8337fde3cb00e
Mock <matheus.mtb7@gmail.com>
2018-08-28 19:50:42 -0300

    Add automated test for rect rendering

---
+++ source/tests/testrectrenderspeed.hpp
@@ -0,0 +1,107 @@
+#include "../settings/definitions.hpp"
+#pragma once
+
+#include "../game/state.hpp"
+#include "../performance/console.hpp"
+#include "../engine/sprite.hpp"
+#include "../engine/text.hpp"
+#include "../engine/renderhelp.hpp"
+#include "../engine/shadermanager.hpp"
+
+#include "../framework/resourcemanager.hpp"
+
+
+class Test_RectRenderSpeed: public State{
+    public:
+        Test_RectRenderSpeed():State(){
+
+            requestQuit = requestDelete = false;
+            duration = 20.0f;
+            state = 0;
+            lg = LineGraph(Point(500,200),200);
+            m_renderTimer = LineGraph(Point(500,200),200);
+            tilesCount = Text("ui/UnB-Office_Regular.ttf",22,TEXT_SOLID,"a",{255,33,180});
+
+        };
+        ~Test_RectRenderSpeed(){
+
+        };
+        void Begin(){
+            ScreenManager::GetInstance().SetScreenName("Test Rect Render Speed");
+        };
+
+
+
+        void Update(float dt){
+            duration -= dt;
+            if( InputManager::GetInstance().IsAnyKeyPressed() != -1 ) {
+                requestDelete = true;
+            }
+
+            if (state >= 2000){
+                return;
+            }
+            static int owo = 0;
+            if (duration <= 0){
+                duration = 0.1f;
+                state++;
+
+
+                if (state % 10 == 0 && state > 0){
+                     owo++;
+
+                    lg.AddData(ScreenManager::GetInstance().GetFps());
+                    m_renderTimer.AddData(renderDuration / (double)state);
+                    std::string txt = utils::format("FPS: %f  : Time to render one call: %f",ScreenManager::GetInstance().GetFps(), renderDuration / (double)state );
+                    tilesCount.SetText(txt);
+                     if (owo >= 4){
+                        bear::out << txt << "\n";
+                     }
+
+                }
+
+            }
+
+
+
+        };
+        void Render(){
+            sw.Reset();
+            //RenderHelp::DrawSquareColor(Rect(0,0,SCREEN_SIZE_W,SCREEN_SIZE_H),100,100,100,255);
+            for (int i=0;i<state;i++){
+                //tiles.SetClip((rand()%10) * 16,(rand()%10) * 16,16,16);
+                //for (int a=1;a<10;a++){
+                    //tiles.Render(Point(  )) ;
+                    RenderHelp::DrawSquareColor(Rect((i % 60) * 16, (i/60)*16,16,16),i%255,120,255 - (i%255),255);
+                //}
+            }
+            //Painter::DrawSprites(tiles.GetTexture().get()->id);
+            renderDuration = (double)sw.Get();
+
+            lg.Render(Point(32,32));
+            m_renderTimer.Render(Point(32,264));
+
+            tilesCount.Render(Point(64,64));
+
+        };
+        void Input();
+        void Resume(){};
+        void End(){
+            Assets.erase();
+            ResourceManager::GetInstance().Erase("test");
+        };
+    private:
+        double renderDuration;
+        Stopwatch sw;
+        LineGraph lg;
+        LineGraph m_renderTimer;
+        Text tilesCount;
+        int state;
+        float duration;
+};
+
+
+
+
+
+
