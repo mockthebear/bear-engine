96552505a0a5f48df39b7a42db3b6fdc5e752de2
Mock <matheus.mtb7@gmail.com>
2018-09-15 15:57:54 -0300

    Adding class RenderTexture
    Now the hierarchy is:
    
    GLtexture -> BearTexture -> RenderTexture -> Sprite

---
+++ source/engine/rendertexture.cpp
@@ -0,0 +1,80 @@
+#include "rendertexture.hpp"
+
+
+
+void RenderTexture::RenderAll(){
+    Painter::RenderTexture(m_texture.get(),m_renderData);
+}
+
+
+Point RenderTexture::GetCenter(){
+    return m_renderData->center;
+}
+
+
+void RenderTexture::SetCenter(Point p){
+    m_renderData->center.x = p.x;
+    m_renderData->center.y = p.y;
+};
+
+
+void RenderTexture::ReBlend(uint8_t Red,uint8_t Blue,uint8_t Green){
+    m_renderData->color[0] = Red/255.0f;
+    m_renderData->color[1] = Blue/255.0f;
+    m_renderData->color[2] = Green/255.0f;
+};
+
+void RenderTexture::SetAlpha(uint8_t alpha){
+    m_renderData->color[3] = alpha/255.0f;
+};
+
+uint8_t RenderTexture::GetAlpha(){
+    return m_renderData->color[3]*255;
+};
+
+
+bool RenderTexture::IsLoaded(){
+    return m_texture.get() != nullptr ;
+};
+
+TexturePtr RenderTexture::GetTexture(){
+    return m_texture;
+};
+
+
+bool RenderTexture::HasTexture(){
+    return m_texture.get() != nullptr;
+}
+
+void RenderTexture::SetTexture(TexturePtr t){
+    m_texture = t;
+};
+
+RenderDataPtr RenderTexture::GetRenderData(){
+    return m_renderData;
+}
+
+void RenderTexture::SetFlip(SDL_RendererFlip flipState){
+    m_renderData->flip = (uint8_t)flipState;
+}
+
+SDL_RendererFlip RenderTexture::GetFlip(){
+    return (SDL_RendererFlip)m_renderData->flip;
+}
+
+
+void RenderTexture::SetScaleX(float scale){
+    m_renderData->GetScale().x=scale;
+};
+
+void RenderTexture::SetScaleY(float scale){
+    m_renderData->GetScale().y=scale;
+};
+
+void RenderTexture::SetScale(Point t_scale){
+    m_renderData->SetScale(t_scale);
+};
+
+Point RenderTexture::GetScale(){
+    return m_renderData->GetScale();;
+};
---
+++ source/engine/rendertexture.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "libheader.hpp"
+#include "../framework/geometry.hpp"
+#include "../framework/chainptr.hpp"
+#include "painters/painters.hpp"
+
+
+class RenderTexture{
+    public:
+        RenderTexture():aliasing(TEXTURE_LINEAR),m_renderData(std::make_shared<RenderData>()),size(1.0f, 1.0f),TextureSize(1.0f, 1.0f){};
+
+        bool HasTexture();
+        void SetTexture(TexturePtr t);
+        RenderDataPtr GetRenderData();
+        void SetFlip(SDL_RendererFlip flipState);
+        SDL_RendererFlip GetFlip();
+        /**
+            *Change the sprite scale. Its an local scale, not shared.
+            @param scale the original value is 1.
+        */
+        void SetScaleX(float scale=1.0f);
+        void SetScaleY(float scale=1.0f);
+        /**
+            *Change the sprite scale. Its an local scale, not shared.
+            @param scale the original value is 1.
+        */
+        void SetScale(Point t_scale = Point(1.0f,1.0f));
+        Point GetScale();
+        /**
+            *You can cut some color channels and reblend the sprite
+            *This changes the texture, so its <b>SHARED</b>
+            *When set something here, if you use the same sprite in another place, its good to restore
+            @param Red [0-255] The default is 255 of all sprites;
+            @param Blue [0-255] The default is 255 of all sprites;
+            @param Green [0-255] The default is 255 of all sprites;
+        */
+        void ReBlend(uint8_t Red,uint8_t Blue,uint8_t Green);
+        /**
+            *Changed the sprite alpha
+            *This changes the texture, so its <b>SHARED</b>
+            *When set something here, if you use the same sprite in another place, its good to restore
+            @param alpha [0-255] The default is 255 of all sprites;
+        */
+        void SetAlpha(uint8_t alpha);
+        uint8_t GetAlpha();
+        bool IsLoaded();
+        /**
+            *Get the texture. Note that texture can be an shared one
+            @return The SDL_Texture pointer;
+        */
+        TexturePtr GetTexture();
+        /**
+            *Used to change the center of rotation
+            @param p Sprite position (inside the image)
+        */
+        void SetCenter(Point p);
+        Point GetCenter();
+        Point GetSize(){ return size;};
+        Point GetTextureSize(){ return TextureSize;};
+        void RenderAll();
+    protected:
+        TextureLoadMethod aliasing;
+        RenderDataPtr m_renderData;
+        Point size;
+        Point TextureSize;
+
+        TexturePtr m_texture;
+};
