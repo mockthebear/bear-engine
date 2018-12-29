7cbc72d0a003981aca627c5c1b970c3968389cb9
Mock <matheus.mtb7@gmail.com>
2018-12-29 05:40:57 -0200

    Generated some pattern in empty texture

--- source/engine/painters/common.cpp
+++ source/engine/painters/common.cpp
@@ -13,9 +13,16 @@ void Painter::SetEmptyTexture(){
     TextureLoadMethod filter;
     filter.mode = TEXTURE_NEAREST;
     uint32_t data[32*32];
-    for (int i=0;i< 32*32; i++){
+    for (uint32_t i=0;i< 32*32; i++){
         uint8_t ran = rand()%255;
-        data[i] = RenderHelp::FormatRGBA(ran,ran,ran,255);
+        uint8_t ranR = ran;
+        uint32_t x = i % 32;
+        uint32_t y = i / 32;
+        if (x == y ||  (32-x) == y || x == 0 || y == 0 || x == 31 || y == 31){
+            ranR = 255;
+            ran = 0;
+        }
+        data[i] = RenderHelp::FormatRGBA(ranR,ran,ran,255);
     }
     st = MakeTexture(Point(32,32),GL_RGBA,(unsigned char*)data,filter);    //UnloadedTextureId
     UnloadedTextureId = st->GLimageId;
