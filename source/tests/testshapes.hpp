e53b35e2a2a5c1c77440a7340792d1224336ae68
Mock <matheus.mtb7@gmail.com>
2019-01-14 19:04:04 -0200

    updating tests

--- source/tests/testshapes.hpp
+++ source/tests/testshapes.hpp
@@ -36,26 +36,44 @@ class Test_Shapes: public State{
             RenderHelp::DrawSquareColor(Rect(98,220,64,64),255,100,100,255,true);
             RenderHelp::DrawSquareColor(Rect(220,220,120,64),255,255,255,255,false);
 
+            BearColor colors[4] = {
+                BearColor(1.0f, 0.0f, 0.0f, 1.0f),
+                BearColor(0.0f, 1.0f, 0.0f, 1.0f),
+                BearColor(0.0f, 0.0f, 1.0f, 1.0f),
+                BearColor(1.0f, 1.0f, 0.0f, 1.0f),
+            };
+            RenderHelp::DrawSquareColor(Rect(320,320,120,64),255,255,255,255,false, 0.0f, colors);
 
-            RenderHelp::DrawSquareColor(Rect(320,320,120,64),255,255,255,255,false);
 
 
-
-            std::vector<Point> lines;
+            std::vector<GameVertice> lines;
             std::vector<Rect> squares;
 
-            for (int i=0;i<16;i++){
+            Point OldP(0, 0);
+
+
+            for (int i=0;i<20;i++){
+                float radAngle = Geometry::toRad( (360.0f / 17.0f) * (float)(i+duration) );
+
+                float rColor = fabs(sin( radAngle ));
+                float bColor = fabs(cos( radAngle ));
+                float gColor = fabs(cos( radAngle ) + sin( radAngle ));
                 float x = 64 + i * 32;
-                float y = 80 + 32*sin( Geometry::toRad( (360.0f / 16.0f) * (float)(i+duration) ) );
-
-                lines.emplace_back(Point(x,y));
-
+                float y = 128 + 32*sin( radAngle );
+                if (i == 0){
+                    OldP.x = x;
+                    OldP.y = y;
+                }
+                lines.emplace_back(GameVertice(OldP.x   ,   OldP.y  , rColor, gColor, bColor, 1.0f));
+                lines.emplace_back(GameVertice(x        ,   y       , rColor, gColor, bColor, 1.0f));
                 squares.emplace_back(Rect(x-16,y-16,32,32));
+                OldP.x = x;
+                OldP.y = y;
             }
 
             RenderHelp::DrawSquaresColor(squares, 40,40,180,120, false);
             RenderHelp::DrawSquaresColor(squares, 255,255,255,150, true);
-            RenderHelp::DrawLinesColor(lines,0,255,0,255,2);
+            RenderHelp::DrawLinesColor(lines,255,255,255,255,2);
 
 
 
